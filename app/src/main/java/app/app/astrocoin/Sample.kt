67cc4baa92d0c45fde825985c94348dc1c651df0
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-22 00:36:20 +0500

    AstroCoin Kotlin new

--- app/src/main/java/app/app/astrocoin/Sample.kt
+++ app/src/main/java/app/app/astrocoin/Sample.kt
@@ -1,12 +1,35 @@
 package app.app.astrocoin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import app.app.astrocoin.databinding.SampleBinding
 
 class Sample : AppCompatActivity() {
+
+    private lateinit var binding: SampleBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sample)
 
+        binding = SampleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_sample)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
