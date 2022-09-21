bc7ca37051aac4ce3fff85a94e922a3d0f34ab59
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-21 15:40:12 +0500

    AstroCoin Kotlin new

--- app/src/main/java/app/app/astrocoin/Password.kt
+++ app/src/main/java/app/app/astrocoin/Password.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class Password : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_password)
+
     }
 }
