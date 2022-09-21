5b63303eff4860943f077bee2ff2b17c05dff8fe
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-21 21:47:41 +0500

    AstroCoin Kotlin new

--- app/src/main/java/app/app/astrocoin/Password.kt
+++ app/src/main/java/app/app/astrocoin/Password.kt
@@ -4,20 +4,20 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.os.Vibrator
+import android.os.Looper
+import android.os.VibrationEffect
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import app.app.astrocoin.models.Getdata
-import app.app.astrocoin.models.LoginRequest
-import app.app.astrocoin.models.TokenRequest
 import com.google.gson.Gson
 
-open class Password : AppCompatActivity() {
+
+class Password : AppCompatActivity() {
 
     private var txtgetname: TextView? = null
     private var txtpassword: TextView? = null
@@ -39,15 +39,17 @@ open class Password : AppCompatActivity() {
 
     private var imgfinger: ImageView? = null
     private var imgback: ImageView? = null
-    var sharedPreferences: SharedPreferences? = null
+    private var sharedPreferences: SharedPreferences? = null
+    
+    private var username = ""
+    private var index = 0
+    private var pasindex = false
+    private var password = ""
+    private var writepass = ""
+    private var newpassword = ""
+    private var click = true
 
-    var username = ""
-    var index = 0
-    var pasindex = false
-    var password = ""
-    var writepass = ""
-    var newpassword = ""
-
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
@@ -75,69 +77,90 @@ open class Password : AppCompatActivity() {
         imgfinger = findViewById(R.id.imglog_finger)
         imgback = findViewById(R.id.imglog_back)
 
-
+        val vibrationEffect = VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE)
         sharedPreferences = getSharedPreferences("astrocoin", Context.MODE_PRIVATE)
-
+        getUserData()
         if (password == "") {
             txtpassword?.text = getString(R.string.create_password)
         } else {
             txtpassword?.text = getString(R.string.enter_password)
         }
 
-        getUserData()
         txtpas1?.setOnClickListener {
-            index++
-            writepass += "1"
-            checkindex()
+            if (click) {
+                index++
+                writepass += "1"
+                checkindex()
+            }
         }
         txtpas2?.setOnClickListener {
-            index++
-            writepass += "2"
-            checkindex()
+            if (click) {
+                index++
+                writepass += "2"
+                checkindex()
+            }
         }
         txtpas3?.setOnClickListener {
-            index++
-            writepass += "3"
-            checkindex()
+            if (click) {
+                index++
+                writepass += "3"
+                checkindex()
+            }
         }
         txtpas4?.setOnClickListener {
-            index++
-            writepass += "4"
-            checkindex()
+            if (click) {
+                index++
+                writepass += "4"
+                checkindex()
+            }
         }
         txtpas5?.setOnClickListener {
-            index++
-            writepass += "5"
-            checkindex()
+            if (click) {
+                index++
+                writepass += "5"
+                checkindex()
+            }
         }
         txtpas6?.setOnClickListener {
-            index++
-            writepass += "6"
-            checkindex()
+            if (click) {
+                index++
+                writepass += "6"
+                checkindex()
+            }
         }
         txtpas7?.setOnClickListener {
-            index++
-            writepass += "7"
-            checkindex()
+            if (click) {
+                index++
+                writepass += "7"
+                checkindex()
+            }
         }
         txtpas8?.setOnClickListener {
-            index++
-            writepass += "8"
-            checkindex()
+            if (click) {
+                index++
+                writepass += "8"
+                checkindex()
+            }
         }
         txtpas9?.setOnClickListener {
-            index++
-            writepass += "9"
-            checkindex()
+            if (click) {
+                index++
+                writepass += "9"
+                checkindex()
+            }
         }
         txtpas0?.setOnClickListener {
-            index++
-            writepass += "0"
-            checkindex()
+            if (click) {
+                index++
+                writepass += "0"
+                checkindex()
+            }
         }
         imgback?.setOnClickListener {
-            index--
-            checkindexback()
+            if (click) {
+                index--
+                checkindexback()
+            }
         }
     }
 
@@ -159,69 +182,91 @@ open class Password : AppCompatActivity() {
                 viewfour?.setBackgroundResource(R.drawable.pascheck)
                 if (password == "" && pasindex) {
                     if (newpassword == writepass) {
-                        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                        vibrator.vibrate(100)
+                        click = false
+                        //vibrator.vibrate(100)
                         writepass = ""
                         index = 0
                         viewone?.setBackgroundResource(R.drawable.passoucses)
                         viewtwo?.setBackgroundResource(R.drawable.passoucses)
                         viewthree?.setBackgroundResource(R.drawable.passoucses)
                         viewfour?.setBackgroundResource(R.drawable.passoucses)
-                        Handler().postDelayed({
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            click = true
                             sharedPreferences?.edit()?.putString("password", newpassword)?.apply()
-                            val intent = Intent(this, Sample::class.java)
-                            startActivity(intent)
+                            startActivity(Intent(this, Sample::class.java))
                             finish()
                         }, 500)
-                        Toast.makeText(this, "Password created", Toast.LENGTH_SHORT).show()
                     } else {
-                        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                        vibrator.vibrate(700)
+                        click = false
+                        //vibrator.vibrate(700)
                         writepass = ""
                         index = 0
                         viewone?.setBackgroundResource(R.drawable.passerror)
                         viewtwo?.setBackgroundResource(R.drawable.passerror)
                         viewthree?.setBackgroundResource(R.drawable.passerror)
                         viewfour?.setBackgroundResource(R.drawable.passerror)
-                        Handler().postDelayed({
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            click = true
                             viewone?.setBackgroundResource(R.drawable.passsign)
                             viewtwo?.setBackgroundResource(R.drawable.passsign)
                             viewthree?.setBackgroundResource(R.drawable.passsign)
                             viewfour?.setBackgroundResource(R.drawable.passsign)
                             txtpassword?.text = getString(R.string.repat_password)
-                            val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                            vibrator.vibrate(100)
+                            //vibrator.vibrate(100)
                         }, 500)
                         Toast.makeText(this, "Password not match", Toast.LENGTH_SHORT).show()
                     }
                 }
                 if (password == "" && !pasindex) {
+                    click = false
                     newpassword = writepass
                     writepass = ""
                     index = 0
                     pasindex = true
-                    Handler().postDelayed({
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        click = true
                         viewone?.setBackgroundResource(R.drawable.passsign)
                         viewtwo?.setBackgroundResource(R.drawable.passsign)
                         viewthree?.setBackgroundResource(R.drawable.passsign)
                         viewfour?.setBackgroundResource(R.drawable.passsign)
                         txtpassword?.text = getString(R.string.repat_password)
-                        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                        vibrator.vibrate(100)
+                       // vibrator.vibrate(100)
                     }, 500)
                 }
-                /*if(password!="" && writepass == password){
-                    val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                    vibrator.vibrate(100)
-                    writepass = ""
-                    index = 0
-                }else{
-                    val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                    vibrator.vibrate(100)
-                    writepass = ""
-                    index = 0
-                    Toast.makeText(this, "Password not match", Toast.LENGTH_SHORT).show()
-                }*/
+                if (password != ""&&!pasindex){
+                    if (writepass == password){
+                        click = false
+                        writepass = ""
+                        index = 0
+                        viewone?.setBackgroundResource(R.drawable.passoucses)
+                        viewtwo?.setBackgroundResource(R.drawable.passoucses)
+                        viewthree?.setBackgroundResource(R.drawable.passoucses)
+                        viewfour?.setBackgroundResource(R.drawable.passoucses)
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            click = true
+                            startActivity(Intent(this, Sample::class.java))
+                            finish()
+                        }, 500)
+                    }else{
+                        click = false
+                        writepass = ""
+                        index = 0
+                        viewone?.setBackgroundResource(R.drawable.passerror)
+                        viewtwo?.setBackgroundResource(R.drawable.passerror)
+                        viewthree?.setBackgroundResource(R.drawable.passerror)
+                        viewfour?.setBackgroundResource(R.drawable.passerror)
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            click = true
+                            viewone?.setBackgroundResource(R.drawable.passsign)
+                            viewtwo?.setBackgroundResource(R.drawable.passsign)
+                            viewthree?.setBackgroundResource(R.drawable.passsign)
+                            viewfour?.setBackgroundResource(R.drawable.passsign)
+                            //vibrator.vibrate(100)
+                        }, 500)
+                        Toast.makeText(this, "Password not match", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
             }
         }
     }
@@ -250,8 +295,6 @@ open class Password : AppCompatActivity() {
                 viewfour?.setBackgroundResource(R.drawable.passsign)
             }
         }
-
-
     }
 
     @SuppressLint("SetTextI18n")
