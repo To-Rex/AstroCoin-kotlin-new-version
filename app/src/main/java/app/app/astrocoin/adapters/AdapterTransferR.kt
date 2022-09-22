98866b910e59bccdfdd5d6d16d3f40d0120942ff
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-22 20:29:45 +0500

    AstroCoin Kotlin new

--- app/src/main/java/app/app/astrocoin/adapters/AdapterTransferR.kt
+++ app/src/main/java/app/app/astrocoin/adapters/AdapterTransferR.kt
@@ -10,17 +10,24 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.view.View
 import android.widget.TextView
+import app.app.astrocoin.models.Getdata
 import com.google.android.material.imageview.ShapeableImageView
+import com.google.gson.Gson
 import java.util.ArrayList
 
 class AdapterTransferR(
     var context: Context,
     var transferRequestArrayList: ArrayList<TransferRequest>
+
 ) : RecyclerView.Adapter<AdapterTransferR.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_transfer, parent, false)
         return ViewHolder(view)
     }
+
+    private var sharedPreferences: SharedPreferences? = null
+    private var wallet = ""
+
 
     @SuppressLint("SetTextI18n", "InflateParams")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -34,44 +41,50 @@ class AdapterTransferR(
             holder.tranferdata.visibility = View.VISIBLE
             holder.tranferdata.text = transferRequest.datatransfer
         }
-        val sharedPreferences =
-            context.getSharedPreferences("dycfvsufgjoafienvk.iso", Context.MODE_PRIVATE)
-        val wallet = sharedPreferences.getString("wallet", "")
-        if (transferRequest.status == "success" && transferRequest.wallet_to != wallet ||
-            transferRequest.status == "failed" && transferRequest.wallet_to == wallet
-        ) {
-            //holder.trimage.setImageResource(R.drawable.ic_component_3__1_);
-            if (transferRequest.amount == "") {
-                holder.trcoin.visibility = View.GONE
-            } else {
-                holder.trcoin.text = "-" + transferRequest.amount + " ASC"
+
+        sharedPreferences = context.getSharedPreferences("astrocoin", Context.MODE_PRIVATE)
+        getUserData()
+
+        if(transferRequest.status == "failed"&&transferRequest.wallet_to == wallet){
+            if (transferRequest.amount==""){
+                holder.trcoin.visibility = View.VISIBLE
+            }else{
+                holder.trcoin.text = transferRequest.amount+" ASC"
+                holder.trimage.setImageResource(R.drawable.transactionsend)
             }
-        } else {
-            if (transferRequest.status == "failed" && transferRequest.wallet_to != wallet) {
-                //holder.trimage.setImageResource(R.drawable.ic_felid);
-                if (transferRequest.amount == "") {
-                    holder.trcoin.visibility = View.GONE
-                } else {
-                    holder.trcoin.text = transferRequest.amount + " ASC"
-                }
-            } else {
-                if (transferRequest.status == "success" && transferRequest.wallet_to == wallet) {
-                    //holder.trimage.setImageResource(R.drawable.ic_soucsess);
-                    if (transferRequest.amount == "") {
-                        holder.trcoin.visibility = View.GONE
-                    } else {
-                        holder.trcoin.text = "+" + transferRequest.amount + " ASC"
-                    }
-                } else {
-                    //holder.trimage.setImageResource(R.drawable.ic_felid);
-                    if (transferRequest.amount == "") {
-                        holder.trcoin.visibility = View.GONE
-                    } else {
-                        holder.trcoin.text = transferRequest.amount + " ASC"
-                    }
-                }
-            }
+            return
         }
+
+        if(transferRequest.status == "failed"&&transferRequest.wallet_to != wallet){
+            if (transferRequest.amount==""){
+                holder.trcoin.visibility = View.VISIBLE
+            }else{
+                holder.trcoin.text = transferRequest.amount+" ASC"
+                holder.trimage.setImageResource(R.drawable.transactionfeild)
+            }
+            return
+        }
+        if(transferRequest.status == "success"&&transferRequest.wallet_to == wallet){
+            if (transferRequest.amount==""){
+                holder.trcoin.visibility = View.VISIBLE
+
+            }else{
+                holder.trcoin.text = transferRequest.amount+" ASC"
+                holder.trimage.setImageResource(R.drawable.transactionsoucses)
+            }
+            return
+        }
+        if (transferRequest.status == "success"&&transferRequest.wallet_to != wallet){
+            if (transferRequest.amount==""){
+                holder.trcoin.visibility = View.VISIBLE
+
+            }else{
+                holder.trcoin.text = transferRequest.amount+" ASC"
+                holder.trimage.setImageResource(R.drawable.transferfeilde)
+            }
+            return
+        }
+
     }
 
     override fun getItemCount(): Int {
@@ -90,5 +103,12 @@ class AdapterTransferR(
             tranferdata = itemView.findViewById(R.id.tranferdata)
             trimage = itemView.findViewById(R.id.trimage)
         }
+    }
+    @SuppressLint("SetTextI18n")
+    private fun getUserData() {
+        val gson = Gson()
+        val json = sharedPreferences?.getString("user", "")
+        val user = gson.fromJson(json, Getdata::class.java)
+        wallet = user.wallet
     }
 }
