c44c9c44603e9f80655321c3795221d9e60f65eb
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-22 22:59:00 +0500

    AstroCoin Kotlin new

---
+++ app/src/main/java/app/app/astrocoin/adapters/AdapterUserSearch.java
@@ -0,0 +1,32 @@
+package app.app.astrocoin.adapters;
+
+import android.widget.BaseAdapter;
+import android.widget.Filterable;
+
+public class AdapterUserSearch extends BaseAdapter implements Filterable {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public android.view.View getView(int position, android.view.View convertView, android.view.ViewGroup parent) {
+            return null;
+        }
+
+        @Override
+        public android.widget.Filter getFilter() {
+            return null;
+        }
+}
