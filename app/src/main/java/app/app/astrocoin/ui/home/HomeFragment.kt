5041a6055dd8b2363a11395c59d5fd4f5162f436
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-24 17:29:47 +0500

    AstroCoin Kotlin new

--- app/src/main/java/app/app/astrocoin/ui/home/HomeFragment.kt
+++ app/src/main/java/app/app/astrocoin/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -20,6 +21,7 @@ import app.app.astrocoin.fragments.FragmentTransfers
 import app.app.astrocoin.models.Getdata
 import app.app.astrocoin.models.TokenRequest
 import app.app.astrocoin.sampleclass.ApiClient
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.tabs.TabLayout
 import com.google.gson.Gson
 import retrofit2.Call
@@ -36,6 +38,7 @@ class HomeFragment : Fragment() {
     private var tabAdapters: TabAdapters? = null
 
     private var txthomebalance: TextView? = null
+    private var imghomereadqr: ImageView? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -45,12 +48,14 @@ class HomeFragment : Fragment() {
 
     }
 
+    @SuppressLint("InflateParams")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewPager = view.findViewById(R.id.viewPager)
         tabLayout = view.findViewById(R.id.tabLayout)
         txthomebalance = view.findViewById(R.id.txthomebalance)
         swipeRefreshLayout = view.findViewById(R.id.swipe_refresh_layout)
+        imghomereadqr = view.findViewById(R.id.imghomereadqr)
 
         sharedPreferences = requireActivity().getSharedPreferences("astrocoin", Context.MODE_PRIVATE)
 
@@ -61,6 +66,13 @@ class HomeFragment : Fragment() {
         }
         tolsAllFun()
 
+        imghomereadqr!!.setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(requireContext())
+            val view = layoutInflater.inflate(R.layout.home_bottom_qrcode, null)
+            bottomSheetDialog.setContentView(view)
+            bottomSheetDialog.show()
+
+        }
     }
 
     private fun tolsAllFun() {
