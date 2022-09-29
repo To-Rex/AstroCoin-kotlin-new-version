a1758557d2d6e278c8c6e7663957ca2c6f327acb
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-29 13:04:48 +0500

    AstroCoin Kotlin new

--- app/src/main/java/app/app/astrocoin/Sample.kt
+++ app/src/main/java/app/app/astrocoin/Sample.kt
@@ -1,20 +1,26 @@
 package app.app.astrocoin
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.os.postDelayed
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
 class Sample : AppCompatActivity() {
-    var resume = true
+    private var handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.sample)
@@ -23,25 +29,27 @@ class Sample : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_sample)
 
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_home,
-            R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
     override fun onStop() {
-        resume = false
-        Handler(Looper.getMainLooper()).postDelayed(
-            {
-                finish()
-            }, 15000
-        )
+        handler.postDelayed(3000) {
+            finish()
+        }
         super.onStop()
     }
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onStart() {
-        resume = true
+        handler.removeCallbacksAndMessages(null)
         super.onStart()
     }
 
