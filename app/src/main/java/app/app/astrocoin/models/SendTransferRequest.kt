ec7510f54a0b4b3db91c7e14e39c23c488b4f02b
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-30 22:54:06 +0500

    astroCoin Kotlin new version

---
+++ app/src/main/java/app/app/astrocoin/models/SendTransferRequest.kt
@@ -0,0 +1,9 @@
+package app.app.astrocoin.models
+
+class SendTransferRequest {
+    var wallet_to: String? = null
+    var amount = 0.0
+    var comment: String? = null
+    var type: String? = null
+    var title: String? = null
+}
